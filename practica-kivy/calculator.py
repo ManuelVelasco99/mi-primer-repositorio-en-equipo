379e41cd0725e1ee53cb09bbb3d96afc39db56c7
7Rena7 <renacorbe@gmail.com>
2024-03-27 17:20:56 -0300

    Primera versión calculadores

---
+++ practica-kivy/calculator.py
@@ -0,0 +1,18 @@
+import kivy
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+
+
+class Calculator(App):
+    pass
+
+
+if __name__ == '__main__':
+    Calculator().run()
+
+# Button
+# Grid Layout
+# Label
